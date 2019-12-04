fb4874f841c54fba8645db339f2f506406296a87
Davoud Hejazi <hejazi.d@husky.neu.edu>
2019-12-03 19:37:06 -0500

    Comments added. The data uploaded.

--- ANN/NN_wave_class_MSELoss_T_2h.py
+++ ANN/NN_wave_class_MSELoss_T_2h.py
@@ -6,7 +6,8 @@
 # so we kept them the way they were. Trainng set and test set are long matrices of 11 
 # columns of transmittances values, one column per filter. The label files are single column
 # vectors containing the labels corresponding to each row of the training/tests sets.
-# The samples should be randomly shuffled before inputing to the code. 
+# The samples should be randomly shuffled before inputing to the code. The suffled data as .mat 
+# files are provided in the Data folder.
 
 # This code can either start training the model from scracth or use already 
 # trained model, depending on whether the "Restore" value is set to False or True (shown below). 
